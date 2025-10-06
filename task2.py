9842af746c182776dcb5dd13fc6c308c848b0898
Ilya Kupriyanov <ilyakupriyanov@MacBook-Pro-Ilya.local>
2025-10-06 20:20:40 +0300

    Homework

---
+++ task2.py
@@ -0,0 +1,28 @@
+# todo: Преобразуйте переменную age и foo в число
+# age = "23"
+# foo = "23abc"
+#
+# Преобразуйте переменную age в Boolean
+# age = "123abc"
+#
+# Преобразуйте переменную flag в Boolean
+# flag = 1
+#
+# Преобразуйте значение в Boolean
+# str_one = "Privet"
+# str_two = ""
+#
+# Преобразуйте значение 0 и 1 в Boolean
+#
+# Преобразуйте False в строку
+
+age = "23"
+age_1 = int(age)
+foo = '23abc'
+foo_1 = int(foo)
+print(age)
+print (foo)
+
+flag = 1
+flag_1 = bool(flag)
+print (flag_1)
